3a6b263e5f3678d0472deb6471f4c7e3066b02a0
Ruis MacDonald <rs660553@dal.ca>
2026-03-30 11:26:45 -0300

    plotting curves) git

--- plot_reacherloca.sh
+++ plot_reacherloca.sh
@@ -31,21 +31,22 @@ export NUMEXPR_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
 python plot_reacherloca.py \
-  --indir  data/reacherloca_fifo/ data/reacherloca_v1_2/ data/reacherloca_v1_5/ data/reacherloca_v1_10/ data/reacherloca_v2/ data/reacherloca_v2_norm/ \
+  --indir  data/reacherloca_fifo/ data/reacherloca_v2_norm_10/ data/reacherloca_v2_norm_20/ \  # data/reacherloca_v1_2/ data/reacherloca_v1_5/ data/reacherloca_v1_10/ data/reacherloca_v2/
   --outdir results/reacherloca \
   --subdir False \
   --xaxis step \
   --yaxis eval_avg_reward \
   --bins 40000 \
-  --methods reacherloca_fifo reacherloca_v1_2  reacherloca_v1_5  reacherloca_v1_10  reacherloca_v2  reacherloca_v2_norm \
+  --methods reacherloca_fifo reacherloca_v2_norm_10 reacherloca_v2_norm_20 \  #reacherloca_v1_2  reacherloca_v1_5  reacherloca_v1_10  reacherloca_v2
   --add none \
   --labels \
     reacherloca_fifo "DreamerV2 + FIFO" \
-    reacherloca_v1_2 "DreamerV2 + LoFoV1 (2)" \
-    reacherloca_v1_5 "DreamerV2 + LoFoV1 (5)" \
-    reacherloca_v1_10 "DreamerV2 + LoFoV1 (10)" \
-    reacherloca_v2 "DreamerV2 + LoFoV2" \
-    reacherloca_v2_norm "DreamerV2 + LoFoV2 (norm)"
+    reacherloca_v2_norm_10 "DreamerV2 + LoFoV2 (norm 10)" \
+    reacherloca_v2_norm_20 "DreamerV2 + LoFoV2 (norm 20)"
+    # reacherloca_v1_2 "DreamerV2 + LoFoV1 (2)" \
+    # reacherloca_v1_5 "DreamerV2 + LoFoV1 (5)" \
+    # reacherloca_v1_10 "DreamerV2 + LoFoV1 (10)" \
+    # reacherloca_v2 "DreamerV2 + LoFoV2" \
 
 # python plot_reacherloca.py \
 #   --indir  data/reacherloca_fifo_10seeds_save/ data/reacherloca_v1_10seeds_save/ ../Dreamer_v2_SDH/data/reacherloca_v2_state_dist_10_seeds/ ../Dreamer_v2_SDH/data/reacherloca_v2_rep_norm_state_dist_10_seeds/ \
