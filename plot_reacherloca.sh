0aa39b8b911185eb0daca94431ed8e047492ba3f
Ruis MacDonald <rs660553@dal.ca>
2026-04-03 11:37:37 -0300

    curves

--- plot_reacherloca.sh
+++ plot_reacherloca.sh
@@ -31,22 +31,7 @@ export NUMEXPR_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
 python plot_reacherloca.py \
-  --indir  data/reacherloca_fifo_2/ data/reacherloca_v1_rad_03 data/reacherloca_v2_64_500 data/reacherloca_v2_norm_64_500 \
-  --outdir results/reacherloca \
-  --subdir False \
-  --xaxis step \
-  --yaxis eval_avg_reward \
-  --bins 40000 \
-  --methods reacherloca_fifo_2 reacherloca_v1_rad_03 reacherloca_v2_64_500 reacherloca_v2_norm_64_500 \
-  --add none \
-  --labels \
-    reacherloca_fifo_2 "DreamerV2 + FIFO" \
-    reacherloca_v1_rad_03 "DreamerV2 + LoFoV1" \
-    reacherloca_v2_64_500 "DreamerV2 + LoFoV2 (no norm)" \
-    reacherloca_v2_norm_64_500 "DreamerV2 + LoFoV2 "
-
-python plot_reacherloca.py \
-  --indir  data/reacherloca_fifo_rew/ data/reacherloca_v1_rad_03_rew data/reacherloca_v2_64_500_rew data/reacherloca_v2_norm_64_500_rew \
+  --indir  data/reacherloca_fifo_rew data/reacherloca_v1_rad_03_rew data/reacherloca_v2_64_500_rew data/reacherloca_v2_norm_64_500_rew \
   --outdir results/reacherloca_rew \
   --subdir False \
   --xaxis step \
@@ -58,7 +43,22 @@ python plot_reacherloca.py \
     reacherloca_fifo_rew "DreamerV2 + FIFO" \
     reacherloca_v1_rad_03_rew "DreamerV2 + LoFoV1" \
     reacherloca_v2_64_500_rew "DreamerV2 + LoFoV2 (no norm)" \
-    reacherloca_v2_norm_64_500_rew "DreamerV2 + LoFoV2 "
+    reacherloca_v2_norm_64_500_rew "DreamerV2 + LoFoV2"
+
+# python plot_reacherloca.py \
+#   --indir  data/reacherloca_fifo_2 data/reacherloca_v1_rad_03 data/reacherloca_v2_64_500 data/reacherloca_v2_norm_64_500 \
+#   --outdir results/reacherloca \
+#   --subdir False \
+#   --xaxis step \
+#   --yaxis eval_avg_reward \
+#   --bins 40000 \
+#   --methods reacherloca_fifo_2 reacherloca_v1_rad_03 reacherloca_v2_64_500 reacherloca_v2_norm_64_500 \
+#   --add none \
+#   --labels \
+#     reacherloca_fifo_2 "DreamerV2 + FIFO" \
+#     reacherloca_v1_rad_03 "DreamerV2 + LoFoV1" \
+#     reacherloca_v2_64_500 "DreamerV2 + LoFoV2 (no norm)" \
+#     reacherloca_v2_norm_64_500 "DreamerV2 + LoFoV2"
 
 
    
